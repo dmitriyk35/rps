f20971ebeaff2af890804589bf9bde1e649cf483
Dmitriy <danny88100@gmail.com>
2020-03-15 15:36:41 -0400

    made code to display images

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+
 const scissors = "https://i.imgur.com/pgjyhIZ.png";
 const rock = "https://i.imgur.com/LghSkIw.png";
 const paper = "https://i.imgur.com/2gsdqvR.png";
 
 const PlayerCard = (props) => {
+  var sign = props.sign;
+  var image = ""
+
+  if (sign == "rock") {
+    image = rock;
+  } else if (sign == "paper") {
+    image = paper;
+  } else {
+    image = scissors
+  }
+
+
   return (
-    <div className="player-card">{props.sign}</div>
+    <div className="player-card">
+      <img src={image}/>
+    </div>
   )
 };
 
